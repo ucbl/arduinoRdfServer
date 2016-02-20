7b3276988e1f97e6d643a11832457b217e445284
unknown <C:\The Bat!>
2016-02-20 18:42:41 +0100

    adaptation Ethernet2

--- WebServer_Hackfest/WebServer_Hackfest.ino
+++ WebServer_Hackfest/WebServer_Hackfest.ino
@@ -19,14 +19,14 @@
 // *** NETWORK CONFIG ***
 
 #include <SPI.h>
-#include <Ethernet.h>
+#include <Ethernet2.h>
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+  0x90, 0xA2, 0xDA, 0x10, 0x18, 0x6F
 };
-IPAddress ip(192, 168, 1, 177);
+IPAddress ip(192, 168, 1, 2);
 
 // Initialize the Ethernet server library
 // with the IP address and port you want to use
@@ -51,103 +51,6 @@ char location[] = "";
 char device[] = "";
 char value[] = "";
 
-// *** SETUP ***
-void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-/*  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-*/
-
-  // start the Ethernet connection and the server:
-  Ethernet.begin(mac, ip);
-  server.begin();
-  Serial.print("server is at ");
-  Serial.println(Ethernet.localIP());
-}
-
-// *** NORMAL FUNCTIONING ***
-void loop() {
-  // listen for incoming clients
-  EthernetClient client = server.available();
-  if (client) {
-    Serial.println("new client");
-//    Serial.flush();
-    // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
-        //Store the first line of the query
-        if(firstLine) {
-          if(c != '\n') {
-            query[queryLength++] = c;
-          } else {
-            query[queryLength] = '\0';
-            firstLine = false;
-          }
-        }
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-          //parse the request
-          byte request = parseRequest();
-          // send the http response header
-          sendOkHeaders(client);
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          // output the value of each analog input pin
-/*          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-            float sensorReading = getTemp(analogChannel);
-            client.print("analog input ");
-            client.print(analogChannel);
-            client.print(" is ");
-            client.print(sensorReading);
-            client.println("<br />");
-          }
-*/          client.print("Query: ");
-          client.println(query);
-          client.print("Request int value: ");
-          client.println(request);
-          client.print("Method: ");
-          client.println(method);
-          client.print("location: ");
-          client.println(location);
-          client.print("device: ");
-          client.println(device);
-          client.print("Value: ");
-          client.println(value);
-          client.println("</html>");
-
-          break;
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        }
-        else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    // give the web browser time to receive the data
-    delay(1);
-    // close the connection:
-    client.stop();
-    Serial.println("client disconnected");
-
-    //Reinit variables for next request:
-    queryLength = 0;
-    firstLine = true;
-    //requestParameters = "";
-    method[0] = location[0] = device[0] = value[0] = '\0';
-  }
-}
-
 // *** MODEL ***
 float getTemp(int analog) {
   //Formula found at https://learn.adafruit.com/tmp36-temperature-sensor/using-a-temp-sensor
@@ -155,7 +58,7 @@ float getTemp(int analog) {
 }
 
 byte parseRequest() {
-  int start = 0; //start indice of the current part
+  int start = 0; //start index of the current part
   int split = 0; //0 for method, 1 for URL and 2 after (protocol)
   for(int i=0; query[i] != '\0' && split < 2; i++) {
       Serial.println();
@@ -209,19 +112,121 @@ byte parseRequest() {
         Sensor / actuator type: 4 bits, according to the configuration
         Value (only for POST & PUT): 0 = value not set, 1 = value set
   */
+  byte result=0;
   if(method == "GET") {
-    byte result = 0;
+    result = 0;
+    
     //For the moment, assume we only want temperature
+  }
+  return result;
+}
+
+
+
+// *** SETUP ***
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+/*  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+*/
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+}
+
+// *** NORMAL FUNCTIONING ***
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+//    Serial.flush();
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        //Store the first line of the query
+        if(firstLine) {
+          if(c != '\n') {
+            query[queryLength++] = c;
+          } else {
+            query[queryLength] = '\0';
+            firstLine = false;
+          }
+        }
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          //parse the request
+          byte request = parseRequest();
+          // send the http response header
+          //*******sendOkHeaders(client);
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: application/json"); //Response will be JSON-LD
+          //client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println("Access-Control-Allow-Origin: *");  //CORS header, as the server is expected to be queried in cross-domain
+          client.println("Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println();
+          //******************************//
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of each analog input pin
+/*          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            float sensorReading = getTemp(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");
+          }
+*/          
+          client.print("Query: ");
+          client.println(query);
+          client.print("Request int value: ");
+          client.println(request);
+          client.print("Method: ");
+          client.println(method);
+          client.print("location: ");
+          client.println(location);
+          client.print("device: ");
+          client.println(device);
+          client.print("Value: ");
+          client.println(value);
+          client.println("</html>");
+
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+
+    //Reinit variables for next request:
+    queryLength = 0;
+    firstLine = true;
+    //requestParameters = "";
+    method[0] = location[0] = device[0] = value[0] = '\0';
   }
 }
 
-// *** VIEW ***
-void sendOkHeaders(EthernetClient client) {
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: application/json"); //Response will be JSON-LD
-  //client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-  client.println("Access-Control-Allow-Origin: *");  //CORS header, as the server is expected to be queried in cross-domain
-  client.println("Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS");
-  client.println("Connection: close");  // the connection will be closed after completion of the response
-  client.println();
-}
+
